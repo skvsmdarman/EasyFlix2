b0c4b3d511f211de6b132edc6290c223b62d6cda
anilove31 <154661617+anilove31@users.noreply.github.com>
2023-12-27 01:41:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function searchMovies() {
   }
 }
 
-function displayResults(results) {
+async function displayResults(results) {
   resultsContainer.innerHTML = '';
 
   if (results.length === 0) {
@@ -27,31 +27,32 @@ function displayResults(results) {
     return;
   }
 
-  results.forEach(result => {
+  for (const result of results) {
     if (result.media_type !== 'person') {
       const videoLink = (result.media_type === 'movie')
         ? `https://vidsrc.to/embed/movie/${result.id}`
         : `https://vidsrc.to/embed/tv/${result.id}/1/1`;
 
-      fetch(videoLink)
-        .then(response => {
-          if (response.status !== 404) {
-            const resultCard = document.createElement('div');
-            resultCard.classList.add('result-card');
-            resultCard.innerHTML = `
-              <img src="https://image.tmdb.org/t/p/w92${result.poster_path}" alt="${result.title || result.name}">
-              <div>
-                <p>${result.title || result.name}</p>
-                <p>${result.media_type} (${getReleaseYear(result)})</p>
-              </div>
-            `;
-            resultCard.addEventListener('click', () => showDetails(result.id, result.media_type));
-            resultsContainer.appendChild(resultCard);
-          }
-        })
-        .catch(error => console.error('Error checking video link:', error));
+      try {
+        const response = await axios.head(videoLink);
+        if (response.status === 200) {
+          const resultCard = document.createElement('div');
+          resultCard.classList.add('result-card');
+          resultCard.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w92${result.poster_path}" alt="${result.title || result.name}">
+            <div>
+              <p>${result.title || result.name}</p>
+              <p>${result.media_type} (${getReleaseYear(result)})</p>
+            </div>
+          `;
+          resultCard.addEventListener('click', () => showDetails(result.id, result.media_type));
+          resultsContainer.appendChild(resultCard);
+        }
+      } catch (error) {
+        console.error('Error checking video link:', error);
+      }
     }
-  });
+  }
 }
 
 function getReleaseYear(result) {
