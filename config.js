c56e87de149d889aca4c1c115318fc9a669413f6
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-01-07 23:34:57 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  apiKey: 'ec98dcc4e185de0a0b10683fcc3b21f3',
-  apiBaseUrl: 'https://api.themoviedb.org/3',
-  videoHostingServiceUrl: 'https://vidsrc.to/embed',
-  maxVideoHeight: 400,
-  aspectRatio: 16 / 9
-};
