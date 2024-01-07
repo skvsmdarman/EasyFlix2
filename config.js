7c54b5cf98af72138c2ee2c215794a2d54e029f3
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-01-07 23:24:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,7 @@
-const apiKey = 'ec98dcc4e185de0a0b10683fcc3b21f3';
-const apiBaseUrl = 'https://api.themoviedb.org/3';
-const videoHostingServiceUrl = 'https://vidsrc.to/embed';
-const maxVideoHeight = 400;
-const aspectRatio = 16 / 9;
-
-module.export {
-  apiKey,
-  apiBaseUrl,
-  videoHostingServiceUrl,
-  maxVideoHeight,
-  aspectRatio
+var config = {
+  apiKey: 'ec98dcc4e185de0a0b10683fcc3b21f3',
+  apiBaseUrl: 'https://api.themoviedb.org/3',
+  videoHostingServiceUrl: 'https://vidsrc.to/embed',
+  maxVideoHeight: 400,
+  aspectRatio: 16 / 9
 };
