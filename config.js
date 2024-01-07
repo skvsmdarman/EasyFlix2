3c43c259f1f727e230c6bc4f713375a0946e1790
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-01-07 23:07:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const apiKey = 'ec98dcc4e185de0a0b10683fcc3b21f3';
+const apiBaseUrl = 'https://api.themoviedb.org/3';
+const videoHostingServiceUrl = 'https://vidsrc.to/embed';
+const maxVideoHeight = 400;
+const aspectRatio = 16 / 9;
+
+export {
+  apiKey,
+  apiBaseUrl,
+  videoHostingServiceUrl,
+  maxVideoHeight,
+  aspectRatio
+};
