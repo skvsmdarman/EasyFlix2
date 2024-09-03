31bdd0d6a32c39e173baa0e8abe66e03d6328b57
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-09-03 23:54:40 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,7 @@
-const apiKey = 'b5241bb6a49b350b54d6ae5ba084cde7';
-const videoBaseUrl = 'https://vidsrc.cc/v2/embed';
+import config from './config.js';
+
+const apiKey = ${config.tmdbKey};
+const videoBaseUrl = ${config.videoUrl};
 const detailsContainer = document.getElementById('details');
 const watchNowButton = document.getElementById('watchNowButton');
 const videoOptionsContainer = document.getElementById('videoOptions');
@@ -22,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function showDetails(id, mediaType, season, episode) {
-  const detailsUrl = `https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${apiKey}`;
+  const detailsUrl = `${config.tmdbUrl}${mediaType}/${id}?api_key=${apiKey}`;
 
   fetch(detailsUrl)
     .then(response => response.json())
@@ -49,7 +51,7 @@ function displayDetails(details, mediaType) {
   const genres = details.genres ? details.genres.map(genre => genre.name).join(', ') : '';
   detailsContainer.innerHTML = `
   <div style="text-align: center; margin-bottom: 20px;">
-    <img src="https://image.tmdb.org/t/p/w300${details.poster_path}" alt="${details.title || details.name}" style="max-width: 100%;">
+    <img src="${config.tmdbPoster}${details.poster_path}" alt="${details.title || details.name}" style="max-width: 100%;">
   </div>
   <h2 style="text-align: center; color: #3498db;">${details.title || details.name}</h2>
   <p><strong>Type:</strong> ${mediaType}</p>
@@ -139,7 +141,7 @@ function updateUrl(id, mediaType, season, episode) {
 
 function updateEpisodeDetails(seriesId, seasonNumber, episodeNumber) {
   videoContainer.innerHTML = '';
-  const episodeDetailsUrl = `https://api.themoviedb.org/3/tv/${seriesId}/season/${seasonNumber}/episode/${episodeNumber}?api_key=${apiKey}`;
+  const episodeDetailsUrl = `${config.tmdbUrl}/tv/${seriesId}/season/${seasonNumber}/episode/${episodeNumber}?api_key=${apiKey}`;
 
   fetch(episodeDetailsUrl)
     .then(response => response.json())
