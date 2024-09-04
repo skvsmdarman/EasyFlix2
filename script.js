07d3c59d9fd70fae56570c4ac73bf7ed2c71ccf0
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-09-04 13:20:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,6 @@ function handleSearchFormSubmit(event) {
   event.preventDefault();
   searchMovies();
 }
+
+window.handleSearchFormSubmit = handleSearchFormSubmit;
+window.searchMovies = searchMovies;
