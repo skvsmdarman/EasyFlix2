8c5be0fd2125856ba1c2c89abf82a64378a94eb6
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-09-04 13:32:08 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-tmdbApiKey: 'b5241bb6a49b350b54d6ae5ba084cde7',
-tmdbBaseUrl: 'https://api.themoviedb.org/3',
-tmdbPosterUrl: 'https://image.tmdb.org/t/p/',
-vidsrcBaseUrl: 'https://vidsrc.cc/v2/embed'
-};
- 
-export default config;
