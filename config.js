f6a79e4b1732fd829604a948fc26181a92988ce1
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-09-04 12:18:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
 tmdbApiKey: 'b5241bb6a49b350b54d6ae5ba084cde7',
 tmdbBaseUrl: 'https://api.themoviedb.org/3',
-tmdbPosterUrl: 'https://image.tmdb.org/t/p/w300',
+tmdbPosterUrl: 'https://image.tmdb.org/t/p/',
 vidsrcBaseUrl: 'https://vidsrc.cc/v2/embed'
 };
  
