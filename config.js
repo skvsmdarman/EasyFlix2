054570bb5508e7afa5dcec6c96afcee5ea49cb6b
anilove31 <154661617+anilove31@users.noreply.github.com>
2024-09-04 12:01:52 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+tmdbApiKey: 'b5241bb6a49b350b54d6ae5ba084cde7',
+tmdbBaseUrl: 'https://api.themoviedb.org/3',
+tmdbPosterUrl: 'https://image.tmdb.org/t/p/w300',
+vidsrcBaseUrl: 'https://vidsrc.cc/v2/embed'
+};
+ 
+export default config;
